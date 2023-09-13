f6cc8a945352dfa2e8a4ae0fed9e9060b065eba5
Leroy Mogire <ntruderleroy@gmail.com>
2023-09-13 12:29:23 +0300

    Vue Props

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+    <section>
+        <header><h2>My friends</h2></header>
+        <ul>
+            <friend-contact
+            v-for="friend in friends"
+            :key="friend.id"
+            :id="friend.id"
+            :name="friend.name"
+            :phone-number="friend.phone"
+            :email-address="friend.email"
+            :is-favourite="friend.isFavourite"
+            @toggle-favourite="toggleFavouriteStatus"
+            ></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Maneul Lorenz',
+                        phone: '0783243223',
+                        email: 'maulel@localhost.com',
+                        isFavourite: true
+                    },
+                    {
+                        id: 'julie',
+                        name: 'julie yohoho', 
+                        phone: '078429738',
+                        email: 'julie@localhost.com',
+                        isFavourite: false
+                    }
+                ]
+            }
+        },
+        methods: { 
+            toggleFavouriteStatus (friendId) {
+                const identifiedFriend = this.friends.find(friend => friend.id === friendId)
+                identifiedFriend.isFavourite = !identifiedFriend.isFavourite
+            }
+        }
+    }
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Jost', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+
+header {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 3rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    background-color: #58004d;
+    color: white;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
+}
+
+#app ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+#app li {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 1rem auto;
+    border-radius: 10px;
+    padding: 1rem;
+    text-align: center;
+    width: 90%;
+    max-width: 40rem;
+}
+
+#app h2 {
+    font-size: 2rem;
+    border-bottom: 4px solid #ccc;
+    color: #58004d;
+    margin: 0 0 1rem 0;
+}
+
+#app button {
+    font: inherit;
+    cursor: pointer;
+    border: 1px solid #ff0077;
+    background-color: #ff0077;
+    color: white;
+    padding: 0.05rem 1rem;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+    background-color: #ec3169;
+    border-color: #ec3169;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ isFavourite ? '(favourite)' : '' }}</h2>
+        <button @click="toggleDetails">Show detqails</button>
+        <button @click="toggleFavourite">toggle favourite</button>
+        <ul v-if="detailsVisible">
+            <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+            <li><strong>Email:</strong>{{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        // props: [
+        // 'name',
+        // 'phoneNumber',
+        // 'emailAddress',
+        // 'isFavourite'
+        // ],
+        props: { 
+            id: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            phoneNumber: {
+                type: String,
+                required: true
+            },
+            emailAddress: {
+                type: String,
+                required: true
+            },
+            isFavourite: {
+                type: Boolean,
+                required: false, 
+                default: false,
+                // validator: function(value) {
+                //     return value === '1' || value === '0'}
+            }
+        },
+        emits: ['toggle-favourite'],
+        data() {
+            return {
+                detailsVisible: false,
+            }
+        },
+        methods: {
+            toggleDetails() {
+                this.detailsVisible = !this.detailsVisible;
+            },
+            toggleFavourite() {
+                this.$emit('toggle-favourite', this.id);
+            }
+        }
+    }
+</script>
